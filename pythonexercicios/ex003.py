390d3c2c347afc743cb6fc8e151882824b6b5730
Davi Roda <daviroda48@gmail.com>
2025-02-19 19:32:25 -0300

    Create installation

---
+++ pythonexercicios/ex003.py
@@ -0,0 +1,4 @@
+primeironumero = int(input('primeiro numero: '))
+segundonumero = int(input('segundo numero: '))
+soma = primeironumero + segundonumero
+print(f'A soma entre {primeironumero} e {segundonumero} é 'f'igual a {soma}!')
