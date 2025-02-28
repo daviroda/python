0cfa56bd12bb438b56d0d938d16307b39cdaf86a
Davi Roda <daviroda48@gmail.com>
2025-02-27 21:15:35 -0300

    atualização

--- pythonexercicios/ex005.py
+++ pythonexercicios/ex005.py
@@ -1,4 +1,2 @@
 number = int(input('Determine um número: '))
-antnumber = number - 1
-sucnumber = number + 1
-print(f'O antessesor de {number} é {antnumber} e o sucessor é {sucnumber}')
+print(f'O antessesor de {number} é {number-1} e o sucessor é {number+1}')
--- pythonexercicios/ex006.py
+++ pythonexercicios/ex006.py
@@ -2,4 +2,4 @@ number = int(input("Determine um número: "))
 dobronumber = number * 2
 triplonumber = number * 3
 raiznumber = number ** (1/2)
-print(f'o {number} tem de dobro: {dobronumber}, de triplo: {triplonumber} e sua raiz quadrada é {raiznumber}')
+print(f'O {number} tem de dobro: {dobronumber} \nDe triplo: {triplonumber} \nE sua raiz quadrada é {raiznumber:.2f}')
