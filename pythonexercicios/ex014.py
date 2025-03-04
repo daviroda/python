51cbe464a2a7e002553ae1c4fd59802ee859f564
Davi Roda <daviroda48@gmail.com>
2025-03-04 12:16:32 -0300

    atualização
    exercício 014

---
+++ pythonexercicios/ex014.py
@@ -0,0 +1,3 @@
+celsius = float(input('Informe a temperatura em °C: '))
+conv_fahrenheit = celsius * 1.8 + 32
+print(f'A temperatura de {celsius:.1f}°C, convertido em fahrenheit temos {conv_fahrenheit:.1f}°F')
