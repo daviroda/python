96ca2cba9e298f745d5e7b296e1ed94040d81fbf
Davi Roda <daviroda48@gmail.com>
2025-05-25 11:41:20 -0300

    Atualização
    finalização do mundo 01

---
+++ pythonteste/aula11.py
@@ -0,0 +1,7 @@
+nome = 'Davi'
+cores = {'limpa':'\033[m',
+         'azul': '\033[4;34m',
+         'amarelo': '\033[33m',
+         'pretoebranco': '\033[7;40m'}
+
+print(f'Olá! Muito prazer em te conhecer, {cores[('pretoebranco')]}{nome}{cores[('limpa')]} !! ')
