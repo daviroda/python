a50f57a8941678ada659eafa78601a6a1e40fb79
Davi Roda <daviroda48@gmail.com>
2026-01-05 15:06:31 -0300

    Create ex076.py

---
+++ pythonexercicios/ex076.py
@@ -0,0 +1,9 @@
+itens = ("Lapis", 1.75, "Borracha", 2.00, "Caderno", 15.90,
+         "Estojo", 25.00, "Transferidor", 4.20, "Compasso", 9.99,
+         "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90)
+print('-'*30)
+print('LISTAGEM DE PREÇOS')
+print('-'*30)
+for c in range (0, len(itens), 2):
+    print(f'{itens[c]} .....R$')
+    print('\n')
