f8d9cf935246065569253ac092ff290604b7d800
Davi Roda <daviroda48@gmail.com>
2026-01-07 13:47:56 -0300

    Update ex076.py

--- pythonexercicios/ex076.py
+++ pythonexercicios/ex076.py
@@ -1,9 +1,18 @@
-itens = ("Lapis", 1.75, "Borracha", 2.00, "Caderno", 15.90,
-         "Estojo", 25.00, "Transferidor", 4.20, "Compasso", 9.99,
-         "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90)
-print('-'*30)
+listagem = ("Lápis", 1.75,
+         "Borracha", 2.00,
+         "Caderno", 15.90,
+         "Estojo", 25.00,
+         "Transferidor", 4.20,
+         "Compasso", 9.99,
+         "Mochila", 120.32,
+         "Canetas", 22.30,
+         "Livro", 34.90)
+print('-' *40)
 print('LISTAGEM DE PREÇOS')
-print('-'*30)
-for c in range (0, len(itens), 2):
-    print(f'{itens[c]} .....R$')
-    print('\n')
+print('-' *40)
+for item in range (0, len(listagem)):
+    if item % 2 == 0:
+        print(f'{listagem[item] :<.30}.......', end='')
+    else:
+        print(f'R${listagem[item] :>7.2f}')
+print('-'*40)
