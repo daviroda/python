73453006a7962b0dd2de54d8a0abdd5616462692
Davi Roda <daviroda48@gmail.com>
2026-01-14 14:35:55 -0300

    Atualização
    Finalização Tuplas - ex077

---
+++ pythonexercicios/ex077.py
@@ -0,0 +1,11 @@
+palavras = ('APRENDER', 'PROGRAMAR',
+            'LINGUAGEM', 'PYTHON',
+            'CURSO', 'GRATIS',
+            'ESTUDAR', 'PRATICAR',
+            'TRABALHAR', 'MERCADO',
+            'PROGRAMADOR', 'FUTURO')
+for palavra in palavras:
+    print(f'\nNa palavra {palavra.upper()} temos vogais ', end=' ')
+    for letra in palavra:
+        if letra in 'AEIOU':
+            print(f'{letra.lower()}', end=' ')
