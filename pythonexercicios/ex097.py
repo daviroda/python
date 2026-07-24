9c800e5f55bc328f656608e84b8a09cb48c4d7db
Davi Roda <daviroda48@gmail.com>
2026-07-24 07:42:05 -0300

    Update
    Ex 097 and Ex 098

---
+++ pythonexercicios/ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# Principal
+escreva('Davi Roda')
+escreva('Curso de Python no YoTube')
+escreva('Curso em Video')
